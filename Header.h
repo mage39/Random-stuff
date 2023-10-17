d9d129334fb7ec44df5ba5cdab002eb1fb3e26dc
mage39 <106197576+mage39@users.noreply.github.com>
2023-10-17 13:39:15 -0500

    ooh, a new branch!

--- Header.h
+++ Header.h
@@ -6,10 +6,10 @@
 #include <stdint.h>
 
 int gcd(int a, int b) {
-	int t = 0;
-
-	while (b != 0) {
-		t = b;
+	a = a > 0 ? a : ~--a;
+	b = b > 0 ? b : ~--b;
+	while (b) {
+		int t = b;
 		b = a % b;
 		a = t;
 	}
