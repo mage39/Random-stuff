ef5da09c9fbdbc28c90c297d318028388c529b0f
mage39 <106197576+mage39@users.noreply.github.com>
2024-09-18 08:58:43 -0500

    update chz

--- c/chz.c
+++ c/chz.c
@@ -6,6 +6,11 @@ typedef struct {
 	char y : 4;
 	char z : 4;
 } frame_t;
+typedef struct {
+	char ID : 4;
+	char skip : 4;
+} frameskip_t;
+#define FRAMESKIP_INIT(a) frameskip_t a = {.ID = -1}
 
 int main () {
 	printf("%d\n", sizeof(frame_t));
