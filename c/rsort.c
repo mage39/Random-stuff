26aebca013e5c95ffdd79eb2fdc9eb9646ec3d1d
mage39 <106197576+mage39@users.noreply.github.com>
2025-01-20 17:05:51 -0600

    so close, and yet so far

--- c/rsort.c
+++ c/rsort.c
@@ -4,57 +4,47 @@
 #include "defines.h"
 
 typedef unsigned uint;
+static constexpr uint radixMask = 0xF;
 
 void randList (uint* list, size_t length) {
 	srand(time(NULL));
-	for (int i = 0; i < length; i++) else list[i] = rand();
+	for (uint i = 0; i < length; i++) list[i] = rand();
 }
 
 void printList (uint* list, size_t length) {
-	for (int i = 0; i < length;) {
+	for (uint i = 0; i < length;) {
 		printf("%8X", list[i]);
 		putchar((++i % 20) ? ' ' : '\n');
 	}
 }
 
-static void swap (uint* a, uint* b) {
-	uint s = *a;
-	*a = *b;
-	*b = s;
-}
-
 static void checkAndSwap (uint* a, uint* b, uint i, uint j) {
 	// this is for branchless shenanigans, maybe
-	bool test = *b & 0xF << i * 4 == j << i * 4;
+	bool test = (*b & radixMask << i * 4) == j << i * 4;
+	if (test) {
+		uint s = *a;
+		*a = *b;
+		*b = s;
+	}
 }
 
-static void fillBucket () {} // i'm trying to reduce the perceived complexity here
-							 // to reduce bugs
-							 // i don't think it's working
+static void fillBucket (uint* list, uint length, uint start, uint end, uint i, uint j) { 
+	forin(k, start, end) forin(l, start, length) {
+		checkAndSwap(&list[k], &list[l], i, j);
+	}
+}
 
 void rsort (uint* list, size_t length) {
+	constexpr uint radix = 16;
 	forin(i, 0, sizeof(uint) * 2) {
 		// ^ the third expression would be qsort's `size` argument
-		uint buckets[16] = {0};
-		forin(j, 0, length) buckets[(list[i] & 0xF << i * 4) >> i * 4]++;
-		for (int j = 1, k = 0; j < 16; j++) {
-			k += buckets[j - 1];
-			buckets[j] += k;
-		}
-		forin(j, 0, *buckets) {
-			forin(k, 0, length) {
-				if (list[k] & 0xF << i * 4 == 0) {
-					swap(&list[j], &list[k]);
-				}
-			}
-		}
-		forin(j, 1, 16) {
-			forin(k, buckets[j - 1], buckets[j]) {
-				forin(l, buckets[j - 1], length) {
-					if (list[l] & 0xF << i * 4 == j << i * 4) {
-						swap(&list[k], &list[l]);
-					}
-				}
+		uint buckets[radix] = {0};
+		forin(j, 0, length) buckets[(list[j] & radixMask << i * 4) >> i * 4]++;
+		forin(j, 1, radix) buckets[j] += buckets[j - 1];
+		if (*buckets) fillBucket(list, length, 0, *buckets, i, 0);
+		forin(j, 1, radix) {
+			if (buckets[j - 1] != buckets[j]) {
+				fillBucket(list, length, buckets[j - 1], buckets[j], i, j);
 			}
 		}
 	}
