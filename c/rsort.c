fd2960073da8181844c24869b9e52b8afcc35687
mage39 <106197576+mage39@users.noreply.github.com>
2025-01-20 20:05:19 -0600

    still no idea, eepy

--- c/rsort.c
+++ c/rsort.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
+#include <stdbit.h>
+#include <limits.h>
 #include <stdio.h>
 #include <time.h>
-#include "defines.h"
+
+#define forin(i, start, stop) for (unsigned i = (start); i < (stop); i++)
 
 typedef unsigned uint;
-static constexpr uint radixMask = 0xF;
+static constexpr uint radix = 16;
+static constexpr uint radixMask = radix - 1;
+static constexpr uint radixMaskWidth = stdc_count_ones(radixMask);
 
 void randList (uint* list, size_t length) {
 	srand(time(NULL));
-	for (uint i = 0; i < length; i++) list[i] = rand();
+	forin(i, 0, length) list[i] = rand();
 }
 
 void printList (uint* list, size_t length) {
@@ -16,30 +21,33 @@ void printList (uint* list, size_t length) {
 		printf("%8X", list[i]);
 		putchar((++i % 20) ? ' ' : '\n');
 	}
+	puts("");
 }
 
 static void checkAndSwap (uint* a, uint* b, uint i, uint j) {
-	// this is for branchless shenanigans, maybe
-	bool test = (*b & radixMask << i * 4) == j << i * 4;
-	if (test) {
+	if ((*b & radixMask << i * radixMaskWidth) == j << i * radixMaskWidth) {
 		uint s = *a;
 		*a = *b;
 		*b = s;
 	}
 }
 
-static void fillBucket (uint* list, uint length, uint start, uint end, uint i, uint j) { 
-	forin(k, start, end) forin(l, start, length) {
-		checkAndSwap(&list[k], &list[l], i, j);
+static void fillBucket (uint* list, uint length, uint start, uint end, uint i, uint j) {
+	forin(k, start, end) {
+		forin(l, start, length) {
+			checkAndSwap(&list[k], &list[l], i, j);
+			break;
+		}
 	}
 }
 
 void rsort (uint* list, size_t length) {
-	constexpr uint radix = 16;
-	forin(i, 0, sizeof(uint) * 2) {
-		// ^ the third expression would be qsort's `size` argument
+	forin(i, 0, sizeof(uint) * (CHAR_BIT / radixMaskWidth)) {
 		uint buckets[radix] = {0};
-		forin(j, 0, length) buckets[(list[j] & radixMask << i * 4) >> i * 4]++;
+		forin(j, 0, length) {
+			uint match = list[j] & radixMask << i * radixMaskWidth;
+			buckets[match >> i * radixMaskWidth]++;
+		}
 		forin(j, 1, radix) buckets[j] += buckets[j - 1];
 		if (*buckets) fillBucket(list, length, 0, *buckets, i, 0);
 		forin(j, 1, radix) {
