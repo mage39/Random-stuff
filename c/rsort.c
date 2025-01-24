2433a76dafd90a590d49587392c78e5d56178c3a
mage39 <106197576+mage39@users.noreply.github.com>
2025-01-23 19:30:26 -0600

    day 3 of this absolute nonsense

--- c/rsort.c
+++ c/rsort.c
@@ -1,5 +1,7 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <stdbit.h>
+#include <string.h>
 #include <limits.h>
 #include <stdio.h>
 #include <time.h>
@@ -21,23 +23,32 @@ void printList (uint* list, size_t length) {
 		printf("%8X", list[i]);
 		putchar((++i % 20) ? ' ' : '\n');
 	}
-	puts("");
 }
 
-static void checkAndSwap (uint* a, uint* b, uint i, uint j) {
-	uint match = *b & radixMask << i * radixMaskWidth;
-	if (match == j << i * radixMaskWidth) {
-		uint s = *a;
-		*a = *b;
-		*b = s;
-	}
+static void swap (uint* a, uint* b) {
+	uint s = *a;
+	*a = *b;
+	*b = s;
 }
 
-static void fillBucket (uint* list, uint length, uint start, uint end, uint i, uint j) {
+static void pushBack (uint* list, size_t length, uint idx) {
+	if (length - idx < 2) return;
+	size_t amount = (length - idx - 1) * sizeof(uint);
+	uint end = list[length - 1];
+	memmove(list + idx + 1, list + idx, amount);
+	list[idx] = end;
+}
+
+static void fillBucket (uint* list, size_t length, uint start, uint end, uint i, uint j) {
+	printf("start: %X, end: %X, searching for: %X\n", start, end, j);
 	forin(k, start, end) {
 		forin(l, start, length) {
-			checkAndSwap(&list[k], &list[l], i, j);
-			break;
+			uint match = list[l] & (radixMask << i * radixMaskWidth);
+			if (match == j << i * radixMaskWidth) {
+				swap(&list[k], &list[l]);
+				pushBack(list, length, k + 1);
+				break;
+			}
 		}
 	}
 }
@@ -46,14 +57,22 @@ void rsort (uint* list, size_t length) {
 	forin(i, 0, sizeof(uint) * (CHAR_BIT / radixMaskWidth)) {
 		uint buckets[radix] = {0};
 		forin(j, 0, length) {
-			uint match = list[j] & radixMask << i * radixMaskWidth;
+			uint match = list[j] & (radixMask << i * radixMaskWidth);
 			buckets[match >> i * radixMaskWidth]++;
 		}
 		forin(j, 1, radix) buckets[j] += buckets[j - 1];
-		if (*buckets) fillBucket(list, length, 0, *buckets, i, 0);
+		printList(buckets, 16);
+		puts("");
+		if (*buckets) {
+			fillBucket(list, length, 0, *buckets, i, 0);
+			printList(list, length);
+			puts(" 0 bucket");
+		}
 		forin(j, 1, radix) {
 			if (buckets[j - 1] != buckets[j]) {
 				fillBucket(list, length, buckets[j - 1], buckets[j], i, j);
+				printList(list, length);
+				printf(" %X bucket\n", j);
 			}
 		}
 	}
