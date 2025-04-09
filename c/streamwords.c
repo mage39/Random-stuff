87631cc25179860e894c30b81d82b8ad450afbfc
mage39 <106197576+mage39@users.noreply.github.com>
2025-04-08 20:07:07 -0500

    if you aint cheatin, you aint tryin

--- c/streamwords.c
+++ c/streamwords.c
@@ -4,12 +4,19 @@
 
 int cmp (const void* a, const void* b) return strncmp(a, b, 1);
 
+int hasCharCount (char* str, char a) {
+	int ret = 0
+	for (unsigned i = 0; i < strlen(str); i++) if (str[i] == a) ret++;
+	return ret;
+}
+
 int main (int argc, char** argv) {
 	if (argc != 2) {
 		puts("usage: ./program <character list>");
 		return 1;
 	}
-	qsort(argv[1], strlen(argv[1]), 1, cmp);
+	size_t argLen = strlen(argv[1]);
+	qsort(argv[1], argLen, 1, cmp);
 	char buf[1024];
 	{
 		char* home = getenv("HOME");
@@ -18,4 +25,8 @@ int main (int argc, char** argv) {
 	strcat(buf, "/Downloads/words.txt");
 	FILE* dict = fopen(buf, "r");
 	while (fgets(buf, sizeof(buf), dict)) {
+		size_t wordLen = strlen(buf);
+		if (wordLen < 4 || wordLen > argLen) continue;
+		
+	}
 }
