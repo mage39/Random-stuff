8afbe86ca38ed89ca1cbbe17660478352ffc6cef
mage39 <106197576+mage39@users.noreply.github.com>
2025-04-09 17:52:26 -0500

    half works (exactly half, which is wierd)

--- c/raytracer.c
+++ c/raytracer.c
@@ -12,6 +12,9 @@ static Vec3 v3add (Vec3 v1, Vec3 v2) {
 	for (int i = 0; i < 3; i++) v1.v[i] += v2.v[i];
 	return v1;
 }
+static Vec3 v3sub (Vec3 v1, Vec3 v2) {
+	return v3add(v1, v3neg(v2));
+}
 static Vec3 v3mul (Vec3 v1, Vec3 v2) {
 	for (int i = 0; i < 3; i++) v1.v[i] *= v2.v[i];
 	return v1;
@@ -42,7 +45,7 @@ static Vec3 v3crs (Vec3 v1, Vec3 v2) {
 static void v3logStr (Vec3 vec, LogStr str) {
 	sprintf(str, "%g %g %g", vec.v[0], vec.v[1], vec.v[2]);
 }
-static void v3toColorLogStr (Vec3 vec, LogStr str) {
+static void v3toColorStr (Vec3 vec, LogStr str) {
 	sprintf(str, "%d %d %d",
 		(int)(vec.v[0] * 255.999),
 		(int)(vec.v[1] * 255.999),
@@ -51,19 +54,43 @@ static void v3toColorLogStr (Vec3 vec, LogStr str) {
 static Vec3 rayAt (Ray a, double t) {
 	return v3add(a.pt, v3sclr(a.dir, t));
 }
+static Vec3 rayColor (Ray r) {
+	double a = 0.5 * (r.dir.v[1] + 1);
+	// expr spec: (1 - a) * (Vec3){{1, 1, 1}} + a * (Vec3){{0.5, 0.7, 1}}
+	return v3add(v3sclr((Vec3){{1, 1, 1}}, (1 - a)),
+				 v3sclr((Vec3){{0.5, 0.7, 1}}, a));
+}
 
 int main (void) {
-	double ratio = 16.0 / 9.0;
+	double ratio = 16.0 / 9.0, focalLength = 1;
 	int width = 400, height = width / ratio;
-	height > 1 ? height : 1;
+	height = height > 1 ? height : 1;
+	double viewHeight = 2, viewWidth = viewHeight * ((double)width / height);
+	Vec3 camCenter = {0};
+	Vec3 viewPortU = {{viewWidth}};
+	Vec3 viewPortV = {{[1] = -viewHeight}};
+	Vec3 pixDeltaU = v3sclr(viewPortU, 1.0 / width);
+	Vec3 pixDeltaV = v3sclr(viewPortV, 1.0 / height);
+	// expr spec: camCenter - (Vec3){{[2] = focalLength}} - viewPortU / 2 - viewPortV / 2
+	Vec3 viewUpperLeft = v3add(camCenter,
+			v3neg(v3add((Vec3){{[2] = focalLength}},
+			v3neg(v3add(v3sclr(viewPortU, 0.5),
+			v3neg(v3sclr(viewPortV, 0.5)))))));
+	// expr spec: viewUpperLeft + 0.5 * (pixDeltaU + pixDeltaV)
+	Vec3 pix00Loc = v3add(viewUpperLeft,
+			v3sclr(v3add(pixDeltaU, pixDeltaV), 0.5));
 	puts("P3");
 	printf("%d %d\n", width, height);
 	puts("255");
 	for (int j = 0; j < width; j++) {
 		for (int i = 0; i < height; i++) {
-			Vec3 color = {{(double)i / (height - 1), (double)j / (width - 1)}};
+			Vec3 pixelCenter = v3add(v3add(pix00Loc, v3sclr(pixDeltaU, i)),
+							   v3sclr(pixDeltaV, j));
+			Vec3 rayDir = v3sub(pixelCenter, camCenter);
+			Ray r = {camCenter, rayDir};
+			Vec3 color = rayColor(r);
 			LogStr buf;
-			v3toColorLogStr(color, buf);
+			v3toColorStr(color, buf);
 			puts(buf);
 		}
 	}
