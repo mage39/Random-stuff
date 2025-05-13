e315398fe6bbf27af26db6bc8d5e54afb8f18a84
mage39 <106197576+mage39@users.noreply.github.com>
2025-05-13 14:54:51 -0500

    figured some things out

--- c/tetris.c
+++ c/tetris.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 enum PieceIdx {
 	SQUARE,
@@ -20,6 +21,7 @@ typedef struct {
 	Vector2 start, end;
 } Line;
 
+static constexpr float E = 1E-5f;
 static constexpr int width = 800;
 static constexpr int height = 1000;
 static constexpr int pixelWidth = 40;
@@ -27,32 +29,32 @@ static constexpr int boardWidth = 10;
 static constexpr int boardHeight = 24;
 // -Wpedantic gives "these are not constants" for the colors
 #pragma GCC diagnostic ignored "-Wpedantic"
-static constexpr Piece pieces[] = { // they're all SIDEWAYS. also, forgot the T piece
+static constexpr Piece pieces[] = { // they're all SIDEWAYS
 	[SQUARE] = {{
-		[3] = {[3] = GREEN, [4] = GREEN},
-		[4] = {[3] = GREEN, [4] = GREEN}}},
+		[2] = {[2] = GREEN, [3] = GREEN},
+		[3] = {[2] = GREEN, [3] = GREEN}}},
 	[LINE] = {{
-		[1] = {[3] = MAROON},
-		[2] = {[3] = MAROON},
-		[3] = {[3] = MAROON},
-		[4] = {[3] = MAROON}}},
+		[1] = {[2] = MAROON},
+		[2] = {[2] = MAROON},
+		[3] = {[2] = MAROON},
+		[4] = {[2] = MAROON}}},
 	[LRIGHT] = {{
-		[2] = {[2] = BLUE, [3] = BLUE},
-		[3] = {[3] = BLUE},
-		[4] = {[3] = BLUE}}},
+		[1] = {[1] = BLUE, [2] = BLUE},
+		[2] = {[2] = BLUE},
+		[3] = {[2] = BLUE}}},
 	[LLEFT] = {{
-		[2] = {[3] = PURPLE, [4] = PURPLE},
-		[3] = {[3] = PURPLE},
-		[4] = {[3] = PURPLE}}},
+		[1] = {[2] = PURPLE, [3] = PURPLE},
+		[2] = {[2] = PURPLE},
+		[3] = {[2] = PURPLE}}},
 	[SRIGHT] = {{
-		[3] = {[3] = ORANGE, [4] = ORANGE},
-		[4] = {[2] = ORANGE, [3] = ORANGE}}},
+		[2] = {[2] = ORANGE, [3] = ORANGE},
+		[3] = {[1] = ORANGE, [2] = ORANGE}}},
 	[SLEFT] = {{
-		[3] = {[2] = PINK, [3] = PINK},
-		[4] = {[3] = PINK, [4] = PINK}}},
+		[2] = {[1] = PINK, [2] = PINK},
+		[3] = {[2] = PINK, [3] = PINK}}},
 	[TPIECE] = {{
-		[3] = {[2] = BEIGE, [3] = BEIGE, [4] = BEIGE},
-		[4] = {[3] = BEIGE}}}
+		[2] = {[1] = BEIGE, [2] = BEIGE, [3] = BEIGE},
+		[3] = {[2] = BEIGE}}}
 };
 #pragma GCC diagnostic pop
 
@@ -90,14 +92,24 @@ static void drawPiece (Image* image, Vector3 center, enum PieceIdx idx) {
 				ImageDrawRectangleRec(image, pixColor, pieces[idx].p[i][j]);
 				ImageDrawRectangleLines(image, pixel, 1, WHITE);
 			}
+			if (i == 2 && j == 2) {
+				Vector2 c = {i * pixelWidth + pixelWidth / 2,
+					j * pixelWidth + pixelWidth / 2};
+				ImageDrawCircleV(image, c, 10, LIME);
+			}
 		}
 	}
 	Texture2D tex = LoadTextureFromImage(*image);
 	DrawTextureEx(tex, (Vector2){center.x, center.y}, center.z, 1, LIGHTGRAY);
 }
 static Vector3 rectifyCenter (Vector3 center) {
-	return (Vector3){500, 80, 0}; // note: imageWidth is 201
+	if (fabsf(center.z - 0) <= E) {
+		center.x -= pixelWidth * 2 + pixelWidth / 2;
+		center.y -= pixelWidth * 2 + pixelWidth / 2;
+	}
+	return center; // note: imageWidth is 201
 }
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 static bool collision (const Color board[boardWidth][boardHeight],
 		Vector3 center, enum PieceIdx which) {
 	return false;
@@ -105,6 +117,7 @@ static bool collision (const Color board[boardWidth][boardHeight],
 static void writeBoard (Color board[boardWidth][boardHeight],
 		Vector3 center, enum PieceIdx which) {
 }
+#pragma GCC diagnostic pop
 static Vector3 update (Vector3 center, float speed) {
 	if (IsKeyPressed(KEY_A)) center.z -= 90;
 	if (IsKeyPressed(KEY_D)) center.z += 90;
@@ -133,7 +146,7 @@ int main (void) {
 	Color board[boardWidth][boardHeight] = {0};
 	Image pieceImage = GenImageColor(imageWidth, imageWidth, BLANK);
 	// enum PieceIdx queue[2] = {0};
-	Vector3 center = {0};
+	Vector3 center = {60 + boardWidth * pixelWidth / 2, 40, 0};
 	InitWindow(width, height, "tetris");
 	while(!WindowShouldClose()) {
 		center = update(center, speed);
@@ -144,7 +157,7 @@ int main (void) {
 		if (collision(board, center, 0)) {
 			writeBoard(board, center, 0);
 			speed += 0.01;
-			center = (Vector3){40, 60 + boardWidth * pixelWidth, 0};
+			center = (Vector3){60 + boardWidth * pixelWidth / 2, 40, 0};
 			// also handle queue things here
 		}
 		// checkLine(board);
